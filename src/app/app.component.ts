572c362d8acc6bf9bc6978fb0e8b83ba45679f6e
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-01-28 22:14:23 +0100

    add isLoggedIn field to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'directorsview';
+
+  public readonly isLoggedIn: Observable<boolean>;
+
+  constructor(private readonly auth: AuthService) {
+    this.isLoggedIn = this.auth.isLoggedInAsObservable;
+  }
 }
