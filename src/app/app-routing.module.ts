3a98785e49f3e9710375de0c1c6473215dd50892
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-01-28 22:14:02 +0100

    implement signup and signin routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'signin',
+    loadChildren: () => import('./views/signin/signin.module').then(m => m.SigninModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./views/signup/signup.module').then(m => m.SignupModule)
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
