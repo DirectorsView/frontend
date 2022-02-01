b7f2f3b620d89e4878521fbb25be420865ed4db0
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-02-01 08:30:46 +0100

    implement backend connection to signupasperson

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { BackendService } from './backend.service';
+import { Company, Person } from '../models/models';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +9,13 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class AuthService {
 
   private readonly loggedInStateSubject: BehaviorSubject<boolean>;
+  public person: Person | null;
+  public company: Company | null;
 
-  constructor() {
+  constructor(private readonly backend: BackendService) {
     this.loggedInStateSubject = new BehaviorSubject<boolean>(false);
+    this.person = null;
+    this.company = null;
   }
 
   public get isLoggedIn(): boolean {
@@ -30,13 +36,36 @@ export class AuthService {
     });
   }
 
-  public signUp(email: string, password: string, firstName?: string, lastName?: string, companyName?: string, accountType?: string): Promise<string> {
-    // TODO: implement backend request and user authentication
+  public signUpAsCompany(email: string, password: string, companyName: string): Promise<string> {
     return new Promise((resolve, reject) => {
-      setTimeout(() => {
+      this.backend.post<Company>('/company', {
+        email,
+        password,
+        companyName
+      }).then(company => {
+        this.company = company;
         this.loggedInStateSubject.next(true);
         resolve('success');
-      }, 1000);
+      }).catch(reason => {
+        reject(reason);
+      });
+    });
+  }
+
+  public signUpAsPerson(email: string, password: string, firstName: string, lastName: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.backend.post<Person>('/person', {
+        email,
+        password,
+        firstName,
+        lastName
+      }).then(person => {
+        this.person = person;
+        this.loggedInStateSubject.next(true);
+        resolve('success');
+      }).catch(reason => {
+        reject(reason);
+      });
     });
   }
 }
