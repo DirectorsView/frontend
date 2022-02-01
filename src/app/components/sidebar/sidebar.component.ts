7c048cb4edf53464d2ece2978da55de324d70984
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-02-01 08:33:00 +0100

    implement sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Project } from '../../models/models';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: [ './sidebar.component.scss' ]
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() public projects: Project[] | null;
+  public sortedProjects: SortedProjects | null;
+
+  constructor() {
+    this.projects = null;
+    this.sortedProjects = null;
+  }
 
   ngOnInit(): void {
   }
 
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['projects']) {
+      this.sortedProjects = this.sortProjects();
+    }
+  }
+
+  public sortProjects(): SortedProjects {
+    if (this.projects === null) {
+      return { active: [], scheduled: [], inactive: [] };
+    }
+
+    const active: Project[] = [];
+    const future: Project[] = [];
+    const past: Project[] = [];
+
+    this.projects.forEach((project: Project) => {
+
+      if (Date.now() < project.endTime.getTime()
+        && Date.now() > project.startTime.getTime()) {
+        active.push(project);
+      } else {
+        if (Date.now() < project.startTime.getTime()) {
+          future.push(project);
+        } else {
+          past.push(project);
+        }
+      }
+    });
+
+    return { active, scheduled: future, inactive: past };
+  }
+}
+
+interface SortedProjects {
+  active: Project[];
+  scheduled: Project[];
+  inactive: Project[];
 }
