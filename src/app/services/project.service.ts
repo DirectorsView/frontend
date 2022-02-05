13847113c065da1f598b5b000515c4ca689c49f0
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-02-05 17:41:22 +0100

    make fetchProjects api call uri dynamic

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Person, Project } from '../models/models';
 import { BackendService } from './backend.service';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,15 @@ export class ProjectService {
 
   private readonly projectsSubject: BehaviorSubject<Project[]>;
 
-  constructor(private readonly backend: BackendService) {
+  constructor(private readonly backend: BackendService,
+              private readonly auth: AuthService) {
     this.projectsSubject = new BehaviorSubject<Project[]>([]);
   }
 
   public fetchProjects(): void {
-    this.backend.get<Project[]>('/person/2/projects').then(projects => {
+    const id = (this.auth.company ? this.auth.company.id : this.auth.person?.id) || -1;
+
+    this.backend.get<Project[]>(`/${ this.auth.company ? 'company' : 'person' }/${ id }/projects`).then(projects => {
       projects.forEach(project => {
         project.startTime = new Date(project.startTime);
         project.endTime = new Date(project.endTime);
@@ -43,5 +47,17 @@ export class ProjectService {
 
   public getProjectMembers(id: number): Promise<Person[]> {
     return this.backend.get<Person[]>(`/project/${ id }/members`);
+  }
+
+  public addProject(project: any): Promise<Project> {
+    return new Promise<Project>(resolve => {
+      this.backend.post<Project>('/project', project).then(project => {
+        project.startTime = new Date(project.startTime);
+        project.endTime = new Date(project.endTime);
+        this.projectsSubject.next([ ...this.projectsSubject.value, project ]);
+      });
+
+      resolve(project);
+    });
   }
 }
