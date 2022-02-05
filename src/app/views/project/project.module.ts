0fe64ed36e11c1681c899f12ac44930eed6f9ba1
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-02-05 17:51:28 +0100

    import modules and import new path

--- src/app/views/project/project.module.ts
+++ src/app/views/project/project.module.ts
@@ -4,11 +4,22 @@ import { ProjectComponent } from './project.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { DetailedComponent } from './detailed/detailed.component';
+import { NewComponent } from './new/new.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
     path: '',
     component: ProjectComponent
+  },
+  {
+    path: 'new',
+    component: NewComponent
   },
   {
     path: ':id',
@@ -19,12 +30,19 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     ProjectComponent,
-    DetailedComponent
+    DetailedComponent,
+    NewComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
   ]
 })
 export class ProjectModule {
