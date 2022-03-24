68e507b29a45ac9b7dd1be59800f865fd07379a9
dorfingerjonas <contact@dorfingerjonas.at>
2022-03-24 22:28:17 +0100

    add chat interfaces

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -7,24 +7,54 @@ export interface Project {
   company: Company,
 }
 
-export interface Company {
-  id: number,
-  name: string
-  email: string,
-  password: string,
+export interface Company extends Account {
+  name: string,
   address: string,
-  website: string,
 }
 
-export interface Person {
-  id: number,
-  email: string,
-  password: string,
-  website: string,
+export interface Person extends Account {
   birthdate: Date,
   firstName: string,
   lastName: string
 }
 
 export interface Employee extends Person {
+}
+
+export interface Account {
+  id: number,
+  password: string,
+  email: string,
+  website: string
+}
+
+export interface ResponseChat {
+  id: number,
+  account1: AccountResponse,
+  account2: AccountResponse,
+  token: string
+}
+
+export interface ChatUser {
+  id: number,
+  name: string
+}
+
+export interface Chat {
+  id: number,
+  name: string,
+  token: string,
+  users: ChatUser[],
+  messages: Message[]
+}
+
+export interface Message {
+  id: number,
+  source: Person | Company,
+  content: string,
+  time: Date,
+  chat: ResponseChat
+}
+
+export interface AccountResponse extends Account, Person, Company {
 }
