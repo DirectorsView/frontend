4065c33dc10256833ab33b1417a397118e88fb1c
dorfingerjonas <contact@dorfingerjonas.at>
2022-03-24 22:28:39 +0100

    check if person is logged in

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: [ './nav.component.scss' ]
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  public isPerson: boolean;
 
-  ngOnInit(): void {
+  constructor(private readonly auth: AuthService) {
+    this.isPerson = false;
   }
 
+  ngOnInit(): void {
+    this.auth.isLoggedInAsObservable.subscribe(() => {
+      this.isPerson = !!this.auth.person;
+    });
+  }
 }
